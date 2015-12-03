8fc2aeff1e50d06d1e97f0575af52b83f5129b74
danielfilho <daniel@vitrolapp.com>
2015-12-03 03:54:56 -0300

    homem bomba, tela que treme

---
+++ _src/Unity/MzRunner/Assets/CameraPadrao.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPadrao : MonoBehaviour {
+
+	public float tremerIntensidade = 0.03f;
+	public float tremerVelocidade = 0.007f;
+	private float tremerFator;
+	private Vector3 posicaoOriginal;
+	private Quaternion rotacaoOriginal;
+	
+	void Update ()
+	{
+		if (tremerFator > 0)
+		{
+			transform.position = posicaoOriginal + Random.insideUnitSphere * tremerFator;
+			transform.rotation = new Quaternion(
+				rotacaoOriginal.x + Random.Range (-tremerFator,tremerFator) * .2f,
+				rotacaoOriginal.y + Random.Range (-tremerFator,tremerFator) * .2f,
+				rotacaoOriginal.z + Random.Range (-tremerFator,tremerFator) * .2f,
+				rotacaoOriginal.w + Random.Range (-tremerFator,tremerFator) * .2f);
+			tremerFator -= tremerVelocidade;
+		}
+	}
+	
+	public void Tremer()
+	{
+		posicaoOriginal = transform.position;
+		rotacaoOriginal = transform.rotation;
+		tremerFator = tremerIntensidade;// .3f;
+	}
+
+}
